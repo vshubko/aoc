3c08da0967f6d23b8af9565979180a34bf64c394
Vasyl Shubko <vasyl.shubko@zalando.de>
2022-12-08 22:08:56 +0100

    day6 added

---
+++ 2022/day6/sol_p2.py
@@ -0,0 +1,18 @@
+import os.path
+
+INPUT_FILE = os.path.join(os.path.dirname(__file__), 'input.txt')
+UNIQUE_CHR_LENGTH = 14
+
+
+def main() -> int:
+    with open(INPUT_FILE) as f:
+        for line in f:
+            for i in range(len(line) - UNIQUE_CHR_LENGTH):
+                if len(set(line[i:(i+UNIQUE_CHR_LENGTH)])) == UNIQUE_CHR_LENGTH:
+                    print(f'res: {i + UNIQUE_CHR_LENGTH}')
+                    break
+    return 0
+
+
+if __name__ == '__main__':
+    raise SystemExit(main())
